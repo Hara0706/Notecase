0d69d6259c0ca2bdbefe66e8454e6f9579025b35
Harapriya Mohapatra <jhanja2000@gmail.com>
2022-02-18 11:04:27 +0530

    Preparing For Deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const path = require('path');
 
 const app = express();
 dotenv.config();
@@ -21,6 +22,22 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
+
+// deployment
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+} else {
+
+    app.get("/", (req, res) => {
+        res.send("API is Running...");
+    });
+
+}
 
 // app.get("/api/notes/:id", (req, res) => {
 //     const note = notes.find((n) => n._id === req.params.id);
